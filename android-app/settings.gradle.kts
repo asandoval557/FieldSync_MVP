43e5c13d5c54742cd52d1a636fe03f5b28ae9eaa
Adrian Sandoval <asandoval2107@gmail.com>
2025-09-06 12:24:32 -0500

    feat(android): initial Compose app (builds with Kotlin 1.9.25, AndroidX on)

---
+++ android-app/settings.gradle.kts
@@ -0,0 +1,7 @@
+﻿pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories { google(); mavenCentral() }
+}
+rootProject.name = "FieldSync"
+include(":app")
