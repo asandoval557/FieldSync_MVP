d1dd7b69165af58a99c05d6b3565aeda614efe0e
Adrian Sandoval <asandoval2107@gmail.com>
2025-09-22 21:11:36 -0500

    Updating StoreManagement.kt to use firebase, update gradle files and add google.json

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,6 +1,7 @@
 ﻿plugins {
-  id("com.android.application") version "8.6.0-alpha07"
-  kotlin("android") version "1.9.25"
+  id("com.android.application")
+  kotlin("android")
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -85,5 +86,7 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.2.1")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+  implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+  implementation("com.google.firebase:firebase-firestore-ktx")
 }
 
